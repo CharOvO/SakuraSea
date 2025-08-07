efa6971869fe1a778bea960dc21fb5c5b17c3016
Char <sakura_net@163.com>
2025-08-07 17:48:41 +0800

    添加动态背景,并优化部分细节

--- header.php
+++ header.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <html lang="zh-CN">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo(get_template_directory_uri().'/assets/css/custom.css') ?>">
-    <link rel="stylesheet" href="https://at.alicdn.com/t/c/font_4991188_mbjdlmjt53.css">
-    <script src="<?php echo(get_template_directory_uri().'/assets/js/main.js') ?>" type="module"></script>
+    <link rel="stylesheet" href="<?php echo (get_template_directory_uri() . '/assets/css/custom.css') ?>">
+    <link rel="stylesheet" href="https://at.alicdn.com/t/c/font_4991188_euiujofr1og.css">
+    <script src="<?php echo (get_template_directory_uri() . '/assets/js/main.js') ?>" type="module"></script>
     <title>
         <?php
-            if(is_home()){
-                // bloginfo('name');
-                echo('SakuraSea-');
-                bloginfo('description');
-            }elseif(is_single){
-                the_title();
-                echo('-');
-                bloginfo('name');
-            }
+        if (is_home()) {
+            // bloginfo('name');
+            echo ('SakuraSea-');
+            bloginfo('description');
+        } elseif (is_single()) {
+            the_title();
+            echo ('-');
+            bloginfo('name');
+        }
         ?>
     </title>
 </head>
+
 <body>
+    <!-- 背景 -->
+    <video autoplay muted loop playsinline id="bg-video">
+        <source src="<?php echo (get_template_directory_uri() . '/assets/video/bg-video-13.mp4') ?>" type="video/mp4">
+    </video>
     <header class="header">
         <!-- 导航栏 -->
         <div class="hdr-nav">
@@ -29,10 +35,10 @@
                     <span>开始浏览</span>
                     <!-- 二级菜单 -->
 
-                     <ul class="hdr-nav-ul sub">
+                    <ul class="hdr-nav-ul sub">
                         <li class="hdr-nav-item sub">
                             <a href="#"><i class="iconfont icon-template"></i>专题</a>
-                             <!-- 三级菜单 -->
+                            <!-- 三级菜单 -->
                             <ul class="hdr-nav-ul sub-sub">
                                 <li class="hdr-nav-item sub-sub"><a href="#">HTML</a></li>
                                 <li class="hdr-nav-item sub-sub"><a href="#">CSS</a></li>
@@ -50,7 +56,7 @@
                                 <li class="hdr-nav-item sub-sub"><a href="#">...</a></li>
                             </ul>
                         </li>
-                        
+
                         <li class="hdr-nav-item sub">
                             <a href="#"><i class="iconfont icon-lable"></i>标签</a>
                             <ul class="hdr-nav-ul sub-sub">
@@ -63,14 +69,14 @@
                         <li class="hdr-nav-item sub">
                             <a href="#"><i class="iconfont icon-one-stop-service"></i>归档</a>
                         </li>
-                     </ul>
+                    </ul>
                 </li>
                 <li class="hdr-nav-item sup">
                     <a href="">我的小窝</a>
                     <ul class="hdr-nav-ul sub">
                         <li class="hdr-nav-item sub">
                             <a href="#"><i class="iconfont icon-template"></i>专题</a>
-                             <!-- 三级菜单 -->
+                            <!-- 三级菜单 -->
                             <ul class="hdr-nav-ul sub-sub">
                                 <li class="hdr-nav-item sub-sub"><a href="#">HTML</a></li>
                                 <li class="hdr-nav-item sub-sub"><a href="#">CSS</a></li>
@@ -88,15 +94,15 @@
                                 <li class="hdr-nav-item sub-sub"><a href="#">...</a></li>
                             </ul>
                         </li>
-                        
-                     </ul>
+
+                    </ul>
                 </li>
                 <li class="hdr-nav-item sup">
                     <a href="">小工具</a>
                     <ul class="hdr-nav-ul sub">
                         <li class="hdr-nav-item sub">
                             <a href="#"><i class="iconfont icon-template"></i>专题</a>
-                             <!-- 三级菜单 -->
+                            <!-- 三级菜单 -->
                             <ul class="hdr-nav-ul sub-sub">
                                 <li class="hdr-nav-item sub-sub"><a href="#">HTML</a></li>
                                 <li class="hdr-nav-item sub-sub"><a href="#">CSS</a></li>
@@ -114,26 +120,25 @@
                                 <li class="hdr-nav-item sub-sub"><a href="#">...</a></li>
                             </ul>
                         </li>
-                        
-                     </ul>
+
+                    </ul>
                 </li>
                 <li class="hdr-nav-item sup"><a href="">友链</a></li>
                 <li class="hdr-nav-item sup"><a href="">关于</a></li>
-             
+
 
             </ul>
         </div>
-       
+
         <!-- logo -->
         <div class="hdr-logo">
-            <a href="<?php bloginfo('url') ?>"><i class="iconfont icon-home-filling"></i><img src="<?php echo(get_template_directory_uri().'/assets/img/logo.png') ?>" alt="logo"></a>
+            <a href="<?php bloginfo('url') ?>"><i class="iconfont icon-home-filling"></i><img src="<?php echo (get_template_directory_uri() . '/assets/img/logo.png') ?>" alt="logo"></a>
             <!-- <a href="<?php bloginfo('url') ?>"><i class="iconfont icon-ziyuan"></i></a> -->
-           
+
         </div>
         <!-- menu -->
-         <menu class="hdr-menu">
+        <menu class="hdr-menu">
 
-         </menu>
+        </menu>
     </header>
     <div class="body-shadow"></div>
-
--- home.php
+++ home.php
@@ -6,32 +6,17 @@
 <?php 
 while ( have_posts() ) : 
     the_post();
-    
-    // 获取特色图片 URL，如果没有则使用默认图片
-    $thumbnail_url = get_the_post_thumbnail_url( get_the_ID(), 'medium' );
-    $default_thumbnail = get_template_directory_uri() . '/assets/img/logo.png'; // 默认图片路径
-    $final_thumbnail = $thumbnail_url ? $thumbnail_url : $default_thumbnail;
-    ?>
-    
-    <div <?php post_class( 'post-item' ); ?>>
-        <div class="post-cover">
-            <img src="<?php echo esc_url( $final_thumbnail ); ?>" alt="<?php the_title_attribute(); ?>" loading="lazy" />
-            <div class="cover-shadow"></div>
-        </div>
+    get_template_part('template-parts/posts');
+?>  
 
-        <div class="post-meta">
-            <div class="post-title">『<?php the_title(); ?>』</div>
-            <!-- <div class="post-description"><?php the_excerpt(); ?></div> -->
-             <div class="post-meta-info">
-                <time class="meta-time" datetime="<?php echo get_the_date( 'c' ); ?>"><i class="iconfont icon-calendar"></i><?php the_time( 'Y-m-d' ); ?></time>
-                <div class="h-line"></div>
-                <div class="meta-author"><i class="iconfont icon-user"></i><?php the_author(); ?></div>
-                <a href="<?php the_permalink(); ?>" class="read-details" aria-label="查看详细"><i class="iconfont icon-view-fill"></i></a>
-             </div>
-            
-        </div>
-    </div>
-<?php endwhile; ?>
+<?php
+    endwhile; 
+    the_posts_pagination(array(
+        'mid_size' => 4,
+        'prev_text' => '<i class="iconfont icon-left-double-arrow"></i>',
+        'next_text' => '<i class="iconfont icon-right-double-arrow"></i>'
+    ));
+?>
     </div>
     <div class="wrapper-l white">
         <pre>
