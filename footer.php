fc0671d7c8f7882a5c53b94c647147c1e88e1cba
Char <sakura_net@163.com>
2025-08-09 21:39:01 +0800

    格式优化

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
     <footer class="footer">
+        <?php wp_footer() ?>
         <div class="copyright">©桜海云版权所有·<a href="#" taget="_block" >SakuraSea</a>主题</div>
         <div class="power">
             <a class="icp" href="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_block"><img src="<?php echo(get_template_directory_uri().'/assets/img/icp.webp') ?>" alt="ICP备案">蜀ICP备2025146653号-1</a>
--- functions.php
+++ functions.php
@@ -1,7 +1,49 @@
-<?php 
+<?php
+/** */
+// 载入主题 
+add_filter('show_admin_bar','__return_false');// 取消工具栏
+add_action('after_setup_theme','skr_setup_theme');
+function skr_setup_theme(){
+    // 主题功能
+    // 注册菜单
     register_nav_menus(array(
-        'header_menu' => '顶部菜单',
-        'footer_menu' => '页脚菜单'
-    ))
+        'primary' => '顶部菜单',
+        'footer' => '页脚菜单',
+        'user' => '用户菜单'
+    ));
+}
+
+//资源加载
+add_action('wp_enqueue_scripts', 'skr_resource');
+function skr_resource()
+{
+
+    // 1. 加载CSS文件
+    wp_enqueue_style(
+        'skr-custom', // 添加前缀避免冲突
+        get_theme_file_uri('/assets/css/custom.css'),
+        array(), // 无依赖
+        false, // 版本控制
+        'all' // 媒体类型
+    );
+
+    // 2. 加载ICON FONT（CDN资源）
+    wp_enqueue_style(
+        'skr-icon-font',
+        'https://at.alicdn.com/t/c/font_4991188_euiujofr1og.css',
+        array(),
+        null, // CDN资源不适用版本控制
+        'all'
+    );
+
+    // 3. 加载JS文件
+    wp_enqueue_script(
+        'skr-main',
+        get_theme_file_uri('/assets/js/main.js'),
+        array(), 
+        false,
+        true // 在页脚加载
+    );
+}
 
 ?>
