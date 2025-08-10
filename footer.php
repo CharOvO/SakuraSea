17d648a9131ff21ee60bacc0350d753062cebb86
Char <sakura_net@163.com>
2025-08-10 17:13:59 +0800

    footer结构重构10%(版权,备案信息动态生成遗留)

--- footer.php
+++ footer.php
@@ -1,9 +1,27 @@
-    <footer class="footer">
+<?php
+/**
+ * The footer for our theme
+ *
+ * This is the template that displays footer
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package SakuraSea
+ * 
+ * TODO:
+ * 1.copyright
+ * 2.icp-box
+ * 
+ */   
+?>
+   <footer class="footer">
         <?php wp_footer() ?>
+        <!-- 版权信息 -->
         <div class="copyright">©桜海云版权所有·<a href="#" taget="_block" >SakuraSea</a>主题</div>
-        <div class="power">
-            <a class="icp" href="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_block"><img src="<?php echo(get_template_directory_uri().'/assets/img/icp.webp') ?>" alt="ICP备案">蜀ICP备2025146653号-1</a>
-            <a class="bean" href="https://beian.mps.gov.cn/#/query/webSearch?code=51011202001113" rel="noreferrer" target="_blank"><img src="<?php echo(get_template_directory_uri().'/assets/img/bean.png') ?>" alt="公安备案">川公网安备51011202001113号</a>
+        <!-- 备案信息 -->
+        <div class="icp-box">
+            <a class="icp" href="https://beian.miit.gov.cn/#/Integrated/recordQuery" target="_block"><img src="<?php echo(get_theme_file_uri().'/assets/img/icp.webp') ?>" alt="ICP备案">蜀ICP备2025146653号-1</a>
+            <a class="bean" href="https://beian.mps.gov.cn/#/query/webSearch?code=51011202001113" rel="noreferrer" target="_blank"><img src="<?php echo(get_theme_file_uri().'/assets/img/bean.png') ?>" alt="公安备案">川公网安备51011202001113号</a>
         </div>
     </footer>
 </body>
